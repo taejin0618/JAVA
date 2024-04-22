c5bdae59ed3afc5a55bd0706a509718aa9d85dd5
taejin0618 <taejin618@daum.net>
2024-04-22 12:57:05 +0900

    commit

---
+++ study/src/ref/NullMain4.java
@@ -0,0 +1,14 @@
+package ref;
+
+public class NullMain4 {
+    public static void main(String[] args) {
+        BigData bigData = new BigData();
+//        BigData.data = new Data();
+
+        System.out.println("bigData = " + bigData.count);
+        System.out.println("bigData = " + bigData.data);
+        System.out.println("bigDta = " + bigData.data.value);
+
+        
+    }
+}
