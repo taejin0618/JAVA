12992c9f39c285ac2bebbe9177a84155578d2917
taejin0618 <taejin618@daum.net>
2024-05-29 13:03:20 +0900

    commit

---
+++ study/src/access/b/BankAccountMain.java
@@ -0,0 +1,12 @@
+package access.b;
+
+import access.BankAccount;
+
+public class BankAccountMain {
+    public static void main(String[] args) {
+
+        BankAccount account = new BankAccount();
+        account.deposit(1000);
+        
+    }
+}
