d40c3562676e2a868d072621d583f5981a676882
taejin <taejin618@daum.net>
2024-06-14 10:39:11 +0900

    commint

--- study/src/static1/DataCountMain3.java
+++ study/src/static1/DataCountMain3.java
@@ -5,10 +5,10 @@ public class DataCountMain3 {
         Data3 data1 = new Data3("A");
         System.out.println("A count=" + Data3.count);
 
-        Data3 data2 = new Data3("A");
-        System.out.println("A count=" + Data3.count);
+        Data3 data2 = new Data3("B");
+        System.out.println("B count=" + Data3.count);
 
-        Data3 data3 = new Data3("A");
-        System.out.println("A count=" + Data3.count);
+        Data3 data3 = new Data3("C");
+        System.out.println("C count=" + Data3.count);
     }
 }
