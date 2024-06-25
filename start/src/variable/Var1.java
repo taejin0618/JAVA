6dbdbd3f598f710585d6d14fa69c9f4d4e1deca4
taejin0618 <taejin618@daum.net>
2024-06-25 12:53:51 +0900

    commit

--- start/src/variable/Var1.java
+++ start/src/variable/Var1.java
@@ -3,8 +3,10 @@ package variable;
 public class Var1 {
 
     public static void main(String[] args) {
+        int num = 10;
+        String string = "abc";
         System.out.println(20); //변경 10 -> 20
-        System.out.println(20); //변경 10 -> 20
-        System.out.println(20); //변경 10 -> 2
+        System.out.println(num); //변경 10 -> 20
+        System.out.println("string"); //변경 10 -> 2
     }
 }
