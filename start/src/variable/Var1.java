7f53f77e056a0142fb45820291390f92984132dd
taejin0618 <taejin618@daum.net>
2024-06-25 12:21:25 +0900

    commit

--- start/src/variable/Var1.java
+++ start/src/variable/Var1.java
@@ -5,6 +5,6 @@ public class Var1 {
     public static void main(String[] args) {
         System.out.println(20); //변경 10 -> 20
         System.out.println(20); //변경 10 -> 20
-        System.out.println(20); //변경 10 -> 20
+        System.out.println(20); //변경 10 -> 2
     }
 }
