41b576317dd4cfa40a6dfb18f1551b7c4e90aad5
taejin0618 <taejin618@daum.net>
2024-07-08 13:00:19 +0900

    commit

--- start/src/loop/ex/ForEx3.java
+++ start/src/loop/ex/ForEx3.java
@@ -3,12 +3,15 @@ package loop.ex;
 public class ForEx3 {
 
     public static void main(String[] args) {
-        int max = 100;
+        int max = 100; // max 변수 100 으로 초기화
 
-        int sum = 0;
+        int sum = 0; // sum 변수 0 으로 초기화
+
+        // i 가 max 보다 작거나 같을때까지 반복 max 는 100 이니 100번 반복
         for (int i = 1; i <= max; i++) {
-            sum += i;
+            sum += i; // int 는 최초 1 sum 에 1 대입 후 다시 반복시 sum 에 1을 계속 더하여 100 번 더하기
+            // sum = 1 + 1 + 1 + 1
         }
-        System.out.println(sum);
+        System.out.println(sum); // 1 부터 100 까지 더한 수를 출력
     }
 }
--- start/src/loop/ex/NestedEx1.java
+++ start/src/loop/ex/NestedEx1.java
@@ -3,10 +3,16 @@ package loop.ex;
 public class NestedEx1 {
 
     public static void main(String[] args) {
+
+        // 이중 반복문
+        /*
+        int 를 1초기화
+         */
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= 9; j++) {
                 System.out.println(i + " * " + j + " = " + i * j);
             }
+//            System.out.println();
         }
     }
 }
