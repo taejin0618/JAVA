7d7679188dbd1c50c42e36928ba45348faaae69f
taejin0618 <taejin618@daum.net>
2024-07-18 12:36:06 +0900

    commit

---
+++ study/src/static2/ex/CarMain.java
@@ -0,0 +1,13 @@
+package static2.ex;
+
+public class CarMain {
+    public static void main(String[] args) {
+
+
+        Car car1 = new Car("k3");
+        Car car2 = new Car("k5");
+        Car car3 = new Car("k9");
+
+        Car.showTotalCars();
+    }
+}
