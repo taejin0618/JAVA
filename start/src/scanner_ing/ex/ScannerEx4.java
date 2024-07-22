babd2c55de700f1862a8cb80295f125fe43fce2b
taejin0618 <taejin618@daum.net>
2024-07-23 08:47:18 +0900

    commit

--- start/src/scanner_ing/ex/ScannerEx4.java
+++ start/src/scanner_ing/ex/ScannerEx4.java
@@ -12,7 +12,8 @@ public class ScannerEx4 {
 
         System.out.println(n + "단의 구구단: ");
         for (int i = 1; i <= 9; i++) { // i 는 1, i 가 9보다 작거나 같을때까지 반복 1 ~ 9
-            System.out.println(n + " x " + i + " = " + n * i); // 입력한 정수 n x i = n x i, ex
+            // 입력한 정수 n x i = n * i 값 출력 n 에 2 입력시 2x1 = 2 부터 i 값이 9 보다 같거나 작을때까지 반복하여 출력
+            System.out.println(n + " x " + i + " = " + n * i);
         }
     }
 }
--- start/src/scanner_ing/ex/ScannerEx5.java
+++ start/src/scanner_ing/ex/ScannerEx5.java
@@ -14,18 +14,23 @@ public class ScannerEx5 {
         int num2 = scanner.nextInt();
 
         // num1이 num2보다 큰 경우, 두 숫자를 교환합니다.
-        if (num1 > num2) {
-            int temp = num1;
-            num1 = num2;
-            num2 = temp;
+        if (num1 > num2) { // n1 이 n2 보다 크면 실행
+            int temp = num1; // 임시 변수에 n1 저장
+            num1 = num2; // n1 에 n2 저장
+            num2 = temp; // n2 에 임시값 복사
         }
 
         System.out.print("두 숫자 사이의 모든 정수:");
-        for (int i = num1; i <= num2; i++) {
-            System.out.print(i);
+        for (int i = num1; i <= num2; i++) { // i 에 n1 값 복사, i 가 n2 보다 작거나 같을때까지
+            // n1 이 5 이고 n2 가 7이면 5 ~ 7 까지 반복 5, 6, 7 이니 3버 반복
+            System.out.print(i); // i 는 n1 이니 5 출력
+
+            // i 가 n2 와 같지 않으면 , 출력
             if (i != num2) {
                 System.out.print(",");
             }
+
         }
     }
+
 }
